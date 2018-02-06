9ee9118cd73ebc700c3bc413f071c08cf4bf633f
Lennart Regebro <regebro@gmail.com>
2018-02-06 11:33:43 +0100

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='cipher.longrequest',
-    version='1.1.0',
+    version='1.1.1.dev0',
     url="http://pypi.python.org/pypi/cipher.longrequest/",
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
